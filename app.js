67184e9cfd0be1d8b66b02847a8b0f437f176b52
Hardys <hardyscc@gmail.com>
2017-07-19 20:26:10 +0800

    -

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var authenticationEndpoint = 'https://k4dd5k0t2m.execute-api.us-west-2.amazonaws.com/dev';
+var authenticationEndpoint = 'https://4bwj6nkus6.execute-api.us-east-1.amazonaws.com/dev';
 var contentApiEndpoint = 'https://lpi5i2ybzg.execute-api.us-west-2.amazonaws.com/dev';
 function testToken() {
   var authorizationToken = localStorage.getItem('authorization_token');
